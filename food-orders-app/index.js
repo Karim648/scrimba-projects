5dd7d23f8cbcdd674c4ebb975c5166d59b850d77
Karim648 <151876709+Karim648@users.noreply.github.com>
2025-07-07 20:36:50 -0400

    class name organization

--- food-orders-app/index.js
+++ food-orders-app/index.js
@@ -1,8 +1,8 @@
 import { menuArray } from "./data.js";
 
-
-const selectedItems = [];
-const total = document.querySelector("#total")
+let totalPrice = 0;
+const completeOrder = document.querySelector(".complete-order");
+const orderMessage = document.querySelector(".order-message");
 
 function getItemHtml(menuItems) {
     // goes into the menuArray and puts the info into html using an array method to iterate
@@ -34,7 +34,7 @@ function render(menu) {
 // add event listener to item to add it to total 
 
 render(menuArray);
-let totalPrice = 0;
+
 const addBtn = document.querySelectorAll("#add-btn");
 // gets total price
 addBtn.forEach(function(btn) {
@@ -47,61 +47,87 @@ addBtn.forEach(function(btn) {
         //get item
         const item = parentEl.querySelector("#food-title").textContent;
         total.classList.remove("display");
-        renderTotal(totalPrice, itemPrice, item)
-        remderItems(item)
+        renderTotal(totalPrice)
+        renderItems(item, itemPrice)
+
+        // remove thank you message
+        orderMessage.classList.add("display");
+
     });
 });
 
 
-function renderTotal(totalPrice, itemPrice, item) {
-    total.innerHTML += `
-       <div class="total-section">
-            <h4>Your order</h4>
-            <div class="total-items">        
-                <div class="selected-items">
-                    <h4>${item}</h4>
-                    <button class="remove">remove</button>
-                </div>
-                <h4 class="item-price">${itemPrice}</h4>
-            </div>
-            <div class="total-price">
-                <h2>Total price:</h2>
-                <h4 class="item-price">$${totalPrice}</h4>
-            </div>
-            <button class="complete-order">Complete Order</button> 
-        </div>
-        `;
+
+function renderTotal(totalPrice) {
+    const sum = document.querySelector(".total-price");
+    sum.innerHTML = `
+    <h2>Total price:</h2>
+    <h4 class="total-item-price">$${totalPrice}</h4>
+    `;
 }
 
-function renderItems(item) {
+function renderItems(item, itemPrice) {
     document.querySelector(".total-items").innerHTML += `
         <div class="selected-items">
-            <h4>${item}</h4>
-            <button class="remove">remove</button>            
-        </div>
-        <h4 class="item-price">${itemPrice}</h4>`;
+            <h4 id="selected-item">${item}</h4>
+            <button class="remove" id="remove">remove</button>            
+            <h4 class="item-price" id="selected-price">$${itemPrice}</h4>
+        </div>`;
 }
 
 
-// html to change in js to getTotal
-{/* <section class="total-section" id="total">
-<div class="total">
-    <h4>Your order</h4>
-    <div class="total-items">
-        <div class="selected-items">
-            <h4>Pizza</h4>
-            <button class="remove">remove</button>
-        </div>
-        <h4 class="item-price">14$</h4>
-    </div>
-    <div class="total-price">
-        <h2>Total price:</h2>
-        <h4 class="item-price">19$</h4>
-    </div>
-    <button class="complete-order">Complete Order</button>
-</div>
-</section> */}
+document.addEventListener("click", function(e) {
+        const removeEl = (e.target.id === "remove") ? e.target.parentElement : ""  // if whatever was clicked has the id of remove then set removeEl to be the parent element of remove else set removeEl to falsy value ""
+        if (removeEl) {
+            const removePrice = Number(removeEl.querySelector("#selected-price").textContent.replace("$", ""));
+            // const removeItem = removeEl.querySelector("#selected-item").textContent;
+            // itemRemoval(removeItem, removePrice)
+            itemRemoval(removeEl, removePrice)
+        }
+    });
 
+function itemRemoval(item, price) {
+    totalPrice -= price;
+    renderTotal(totalPrice);
+    item.remove();  // .remove() removes the element from the DOM
+    if (totalPrice === 0) {
+        document.querySelector("#total").classList.add("display");
+    }
+}
+
+const payment = document.querySelector(".payment")
+
+completeOrder.addEventListener("click", function() {
+    payment.classList.remove("display");
+});
+
+const payBtn = document.querySelector("#pay-btn");
+const paymentForm = document.querySelector(".payment-form");
+
+
+payBtn.addEventListener("click", function(e) {
+    e.preventDefault
+    if (paymentForm.checkValidity()){
+        document.querySelector(".payment").classList.add("display");
+        document.querySelector("#total").classList.add("display");
+        totalPrice = 0;
+        const oldOrder = document.querySelectorAll(".selected-items");
+        oldOrder.forEach(function(order) {  // remove selected items from past order
+            order.remove();
+        })
+        renderMessage(paymentForm.name.value);
+    } 
+    paymentForm.reset();
+});
+
+
+
+function renderMessage(name) {
+    orderMessage.innerHTML = `
+    Thanks, ${name}! Your order is on its way!
+    `;
+    orderMessage.classList.remove("display");
+}
 
 
 
